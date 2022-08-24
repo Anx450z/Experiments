6f2f7d8327da611d3540d817cd1db4706226c0cd
anx450z <anx450z@gmail.com>
2022-08-25 00:32:22 +0530

    add tick-tok

---
+++ tickTok.js
@@ -0,0 +1,43 @@
+class Cell {
+  constructor(position, possibility, value) {
+    this.position = { x: -1, y: -1 };
+    this.value = value;
+    this.possibility = possibility;
+  }
+}
+
+class Board {
+  constructor() {
+    const possibilities = [1 / 3];
+    this.board = new Array(3).fill(-1);
+
+    for (var i = 0; i < 3; i++) {
+      this.board[i] = new Array(3).fill(-1);
+    }
+
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < 3; j++) {
+        const position = {
+          x: i,
+          y: j,
+        };
+        this.board[i][j] = new Cell(position, possibilities, this.board[i][j]);
+      }
+    }
+  }
+
+  show() {
+    for (let i = 0; i < 3; i++) {
+      let rowAnswer = "";
+      for (let j = 0; j < 3; j++) {
+        rowAnswer += this.board[i][j].value + " ";
+      }
+      console.log(rowAnswer);
+    }
+  }
+}
+
+
+b1 = new Board()
+
+b1.show()
